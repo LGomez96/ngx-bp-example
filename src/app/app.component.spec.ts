9e89c414450c9c48a3705e38044bf776146b7746
Juan Diego Garzon <jgarzona@nttdata.com>
2022-08-30 12:11:00 -0500

    cambios clases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('library-bp');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('library-bp app is running!');
-  });
 });
